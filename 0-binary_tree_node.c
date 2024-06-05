0df7c353d9bb67f6b0d1c89f24b227db6e799a3a
root <ihotuagbo@gmail.com>
2024-06-05 13:48:15 -0700

    Binary Tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,7 +6,7 @@
  * @parent: Pointer to the parent node
  * @value: Value to put in the new node
  *  
- * Return: Return NULL on failure, or pointer to the new node 
+ * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
