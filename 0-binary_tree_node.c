f118dd0a9dfdc3e79216225446c08f1ed85dbd4f
root <ihotuagbo@gmail.com>
2024-06-06 16:11:31 -0700

    Binary Trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
@@ -19,6 +18,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
 	return (new_node);
 }
