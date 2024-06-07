1056ab328218910040fb028e751466020d97a74a
root <ihotuagbo@gmail.com>
2024-06-07 14:22:33 -0700

    Binary Trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,11 +13,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent,
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-            return (NULL);
+		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-	    return (NULL);
+	      	return (NULL);
 
 	if (parent->right != NULL)
 	{
@@ -25,5 +25,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent,
 		parent->right->parent = new_node;
 	}
 	parent->right = new_node;
+
 	return (new_node);
 }
