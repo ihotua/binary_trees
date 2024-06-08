b59ed9e7bdf4517caae3d57bde14ca4c375b91b2
root <ihotuagbo@gmail.com>
2024-06-08 14:29:37 -0700

    Binary Trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -21,14 +21,15 @@ int binary_tree_is_leaf(const binary_tree_t *node)
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int leftie, rightie;
+
 	if (tree == NULL)
 		return (0)
 	if (binary_tree_is_leaf(tree))
 		return (1);
 
-	if (tree->left != NULL && tree->right != NULL)
-		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
+	leftie = binary_tree_is_full(tree->left)
+	rightie = binary_tree_is_full(tree->right));
 
 	return (0);
 }
