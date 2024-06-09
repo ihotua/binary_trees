01b725021d86359521a6503b67b9e514978b86c9
root <ihotuagbo@gmail.com>
2024-06-09 14:20:34 -0700

    BInary Trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,18 +6,18 @@
  *
  * Return: if full, 1 else 0. If NULL, return 0
  */
-
+`
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	
+
 	if (tree->left != NULL && tree->right != NULL)
 		return (binary_tree_is_full(tree->left)
 			    && binary_tree_is_full(tree->right));
-	
+
 	return (0);
 }
