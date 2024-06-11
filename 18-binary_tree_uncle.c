4dcbb41720f1d1636e49e0299da8f59a1a07cb30
root <ihotuagbo@gmail.com>
2024-06-11 07:10:12 -0700

    Binary Trees

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,8 +11,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL ||
 	    node->parent->parent == NULL)
 		return (NULL);
-	binary_tree_t *grandparent = node->parent->parent;
-	binary_tree_t *parent = node->parent;
+	
+	grandparent = node->parent->parent;
+	parent = n`ode->parent;
 
 	if (grandparent->left == parent)
 		return (grandparent->right);
