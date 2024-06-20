bc4102990003d38d5eb9ef8c4805142e64c54702
root <ihotuagbo@gmail.com>
2024-06-20 16:28:44 -0700

    Binary Trees

---
+++ 114-bst_remove.c
@@ -0,0 +1,63 @@
+#include "binary_trees.h"
+
+/**
+ * min_value_node - This finds the node with the smallest vlue
+ * @node: The root node of the tree.
+ *
+ * Return: A pointer to the node with the smallest value.
+ */
+bst_t *min_value_node(bst_t *node)
+{
+	bst_t *current = node;
+
+	while (current && current->left != NULL)
+		current = current->left;
+
+	return (current);
+}
+
+/**
+ * bst_remove - Removes a node with a specific value from a BST.
+ * @root: Pointer to the root node of the tree.
+ * @value: The value to remove in the tree.
+ *
+ * Return: A pointer to the new root node of the tree.
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	if (root == NULL)
+		return (NULL);
+
+	if (value < root->n)
+	{
+		root->left = bst_remove(root->left, value);
+	}
+	else if (value > root->n)
+	{
+		root->right = bst_remove(root->right, value);
+	}
+	else
+	{
+		if (root->left == NULL)
+		{
+			bst_t *right_child = root->right;
+			if (right_child != NULL)
+				right_child->parent = root->parent;
+			free(root);
+			return (right_child);
+		}
+		else if (root->right == NULL)
+		{
+			bst_t *left_child = root->left;
+			if (left_child != NULL)
+				left_child->parent = root->parent;
+			free(root);
+			return (left_child);
+		}
+		bst_t *successor = min_value_node(root->right);
+		root->n = successor->n;
+		root->right = bst_remove(root->right, successor->n);
+	}
+
+	return (root);
+}
